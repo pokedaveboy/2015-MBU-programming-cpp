cc0181ea8d8038baf6667aa58f6fca4ab1bc94c2
David (PokeDaveBoy) <davemferrini@gmail.com>
2015-03-14 14:41:02 +0000

    Automatic commit

--- random.cpp
+++ random.cpp
@@ -9,23 +9,46 @@ int main(void){
 	int random_number = 1 + (rand() % 10); // random number between 1 and 10
 	// declare variable to store guess of user and set to 0
 	// declare variable as counter for number of guesses
-
+int guess = 0;
+int count = 0;
 	
 	for(;;)
 	{
 		// increment guess counter
-
+		count=count +1;
 		// read input from user
+		cin >> guess;
 		//
 		
 		// compare input from user to random number
+	
+
+		if (guess == random_number)
+		{
+		 cout << "winner"<< endl; break;
+
+	}
+	else if (guess < random_number)
+		{
+		 cout << "Too Low" << endl;
+		}
+		else  
+		{
+		 cout << "Too High" << endl;
+		
+			
+		}
+	
+
 		// print out winner, too high or too low message
 		//
 
 	}
+	cout << "Number of guesses" << count << endl; 
 
 	// print message with number of guesses
 	//
+	
 	
 
 	return 0;
